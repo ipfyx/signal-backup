b5c811425d7664dc6bc2e609a76299c563292669
ipfyx <ipfyx>
2020-06-24 13:38:04 +0200

    moved argparser to main, new function msg_save

--- signal_save.py
+++ signal_save.py
@@ -9,32 +9,6 @@ from collections import OrderedDict
 from CSS import *
 from signal_structure import MMS, SMS, PART, SMS_SENT, SMS_RECV, SMS_NULL
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--db", dest="db_path", help="Path to signal_backup.db file", type=str)
-parser.add_argument("--attachment", "-a", dest="attachment_dir", help="Path to attachment directory", type=str)
-parser.add_argument("--thread", "-t", dest="thread_id", help="Conversation ID, multiple conversion saving are not yet supported", type=int)
-parser.add_argument("--contact_addr", "-ca", dest="contact_address", help="Contact address, multiple conversion saving are not yet supported", type=int)
-parser.add_argument("--contact_name", "-cn", dest="contact_name", help="Name of the contact you wish to display", type=str)
-parser.add_argument("--you", "-m", dest="your_name", help="Your name", type=str)
-args = parser.parse_args()
-
-#THREAD_ID = 25
-#CONTACT_ADDRESS = 102
-#CONTACT_NAME = 'Gabrielle'
-#MYSELF = 'Florian'
-#PATH_ATTACHMENTS = './out2/attachment/'
-
-THREAD_ID = args.thread_id
-CONTACT_ADDRESS = args.contact_address
-CONTACT_NAME = args.contact_name
-MYSELF = args.your_name
-ATTACHMENT_DIR = args.attachment_dir
-DB_PATH = args.db_path
-
-conn = sqlite3.connect(DB_PATH)
-db_cursor = conn.cursor()
-
 def fetch_contact_msg(contact_address, db_cursor):
   # MMS
   db_cursor.execute("select date, msg_box, body, part_count, quote_id, quote_body, reactions, part._id, part.ct, part.unique_id, part.width, part.height FROM MMS LEFT JOIN part ON part.mid = MMS._id WHERE thread_id={}".format(THREAD_ID))
@@ -98,100 +72,128 @@ def build_msg(contact_name, date, msg, filename=None, contact_quoted=None, quote
 def build_footer():
   return  FOOTER
 
-msg = OrderedDict(sorted(fetch_contact_msg(CONTACT_ADDRESS, db_cursor).items()))
-
 ### TEST ###
-html_result = open('gabrielle.html','w')
-html_result.write(build_header())
+#html_result = open('gabrielle.html','w')
+#html_result.write(build_header())
+#
+#html_result.write(build_msg(CONTACT_NAME, '1583604356792', "Je t'aime", reaction="toto"))
+#html_result.write(build_msg(MYSELF, '1583604356792', "Je t'aime", reaction="tata"))
+#
+#html_result.write(build_msg(CONTACT_NAME, '1583604356792', "Je t'aime", filename='20200315_120238.jpg'))
+#html_result.write(build_msg(MYSELF, '1583604356792', "Je t'aime", filename='20200315_120238.jpg'))
+#
+#html_result.write(build_msg(CONTACT_NAME, '1583604356792', "Je t'aime", contact_quoted=MYSELF, quote="Je t'aime", quote_date='1583604356792'))
+#html_result.write(build_msg(MYSELF, '1583604356792', "Je t'aime", contact_quoted=CONTACT_NAME, quote="Je t'aime", quote_date='1583604356792'))
+#
+#html_result.write(build_msg(CONTACT_NAME, '1583604356792', "Je t'aime", contact_quoted=MYSELF, filename='20200315_120238.jpg', quote="Je t'aime" ,quote_date='1583604356792'))
+#html_result.write(build_msg(MYSELF, '1583604356792', "Je t'aime", contact_quoted=CONTACT_NAME, quote="Je t'aime", filename='20200315_120238.jpg', quote_date='1583604356792'))
+#
+#html_result.write(build_footer())
+#html_result.close()
 
-html_result.write(build_msg(CONTACT_NAME, '1583604356792', "Je t'aime", reaction="toto"))
-html_result.write(build_msg(MYSELF, '1583604356792', "Je t'aime", reaction="tata"))
+def save_msg(output_file, msg_dict):
 
-html_result.write(build_msg(CONTACT_NAME, '1583604356792', "Je t'aime", filename='20200315_120238.jpg'))
-html_result.write(build_msg(MYSELF, '1583604356792', "Je t'aime", filename='20200315_120238.jpg'))
+  html_result = open(output_file,'w')
+  html_result.write(build_header())
 
-html_result.write(build_msg(CONTACT_NAME, '1583604356792', "Je t'aime", contact_quoted=MYSELF, quote="Je t'aime", quote_date='1583604356792'))
-html_result.write(build_msg(MYSELF, '1583604356792', "Je t'aime", contact_quoted=CONTACT_NAME, quote="Je t'aime", quote_date='1583604356792'))
-
-html_result.write(build_msg(CONTACT_NAME, '1583604356792', "Je t'aime", contact_quoted=MYSELF, filename='20200315_120238.jpg', quote="Je t'aime" ,quote_date='1583604356792'))
-html_result.write(build_msg(MYSELF, '1583604356792', "Je t'aime", contact_quoted=CONTACT_NAME, quote="Je t'aime", filename='20200315_120238.jpg', quote_date='1583604356792'))
-
-html_result.write(build_footer())
-html_result.close()
-
-### TEST2 ###
-
-html_result = open('test.html','w')
-html_result.write(build_header())
-
-for msg_key, msgi in msg.items():
-  msg_date = datetime.fromtimestamp(msg_key//1000)
-
-  if isinstance(msgi, SMS):
-    if msgi.sms_type == SMS_RECV:
-      html_result.write(build_msg(CONTACT_NAME, msg_date, msgi.body, reaction=msgi.reactions))
-    elif msgi.sms_type == SMS_SENT:
-      html_result.write(build_msg(MYSELF, msg_date, msgi.body, reaction=msgi.reactions))
-    elif msgi.sms_type in SMS_NULL:
-      pass
-    else:
-      raise ValueError(msgi.sms_type)
-
-  elif isinstance(msgi, MMS):
-    # MMS recieved
-    if msgi.mms_type == SMS_RECV:
-      # MMS is quoting a msg
-      if msgi.quote_id > 0:
-        quoted_msg = msg.get(msgi.quote_id)
-        # MMS can quote an SMS
-        if isinstance(quoted_msg, SMS):
-          html_result.write(build_msg(CONTACT_NAME, msg_date, msgi.body, contact_quoted=MYSELF, quote=msgi.quote_body, quote_date=quoted_msg.date, reaction=msgi.reactions))
-        # MMS can quote an MMS
-        elif isinstance(quoted_msg, MMS):
-          # MMS quote an MMS with an image
-          if quoted_msg.date in msg.keys() and quoted_msg.filename is not None:
-            html_result.write(build_msg(CONTACT_NAME, msg_date, msgi.body, contact_quoted=MYSELF, filename=ATTACHMENT_DIR + quoted_msg.filename, quote= msgi.quote_body, quote_date=quoted_msg.date, reaction=msgi.reactions))
-          # MMS quote an MMS without an image
-          else:
+  for msg_key, msgi in msg_dict.items():
+    msg_date = datetime.fromtimestamp(msg_key//1000)
+  
+    if isinstance(msgi, SMS):
+      if msgi.sms_type == SMS_RECV:
+        html_result.write(build_msg(CONTACT_NAME, msg_date, msgi.body, reaction=msgi.reactions))
+      elif msgi.sms_type == SMS_SENT:
+        html_result.write(build_msg(MYSELF, msg_date, msgi.body, reaction=msgi.reactions))
+      elif msgi.sms_type in SMS_NULL:
+        pass
+      else:
+        raise ValueError(msgi.sms_type)
+  
+    elif isinstance(msgi, MMS):
+      # MMS recieved
+      if msgi.mms_type == SMS_RECV:
+        # MMS is quoting a msg
+        if msgi.quote_id > 0:
+          quoted_msg = msg_dict.get(msgi.quote_id)
+          # MMS can quote an SMS
+          if isinstance(quoted_msg, SMS):
             html_result.write(build_msg(CONTACT_NAME, msg_date, msgi.body, contact_quoted=MYSELF, quote=msgi.quote_body, quote_date=quoted_msg.date, reaction=msgi.reactions))
-        else:
-          html_result.write(build_msg(CONTACT_NAME, msg_date, msgi.body, contact_quoted=MYSELF, quote="NULL quote", quote_date=None, reaction=msgi.reactions))
-      # MMS is embedding a simple MMS without quoting
-      elif msgi.filename is not None:
-        html_result.write(build_msg(CONTACT_NAME, msg_date, msgi.body, filename=ATTACHMENT_DIR + msgi.filename, reaction=msgi.reactions))
-      else:
-        raise ValueError
-
-    # MMS sent
-    elif msgi.mms_type == SMS_SENT:
-      # MMS is quoting a msg
-      if msgi.quote_id > 0:
-        quoted_msg = msg.get(msgi.quote_id)
-        # MMS can quote an SMS
-        if isinstance(quoted_msg, SMS):
-          html_result.write(build_msg(MYSELF, msg_date, msgi.body, contact_quoted=CONTACT_NAME, quote=msgi.quote_body, quote_date=quoted_msg.date, reaction=msgi.reactions))
-        # MMS can quote an MMS
-        elif isinstance(quoted_msg, MMS):
-          # MMS quote an MMS with an image
-          if quoted_msg.date in msg.keys() and quoted_msg.filename is not None:
-            html_result.write(build_msg(MYSELF, msg_date, msgi.body, contact_quoted=CONTACT_NAME, filename=ATTACHMENT_DIR + quoted_msg.filename, quote= msgi.quote_body, quote_date=quoted_msg.date, reaction=msgi.reactions))
-          # MMS quote an MMS without an image
+          # MMS can quote an MMS
+          elif isinstance(quoted_msg, MMS):
+            # MMS quote an MMS with an image
+            if quoted_msg.date in msg_dict.keys() and quoted_msg.filename is not None:
+              html_result.write(build_msg(CONTACT_NAME, msg_date, msgi.body, contact_quoted=MYSELF, filename=ATTACHMENT_DIR + quoted_msg.filename, quote= msgi.quote_body, quote_date=quoted_msg.date, reaction=msgi.reactions))
+            # MMS quote an MMS without an image
+            else:
+              html_result.write(build_msg(CONTACT_NAME, msg_date, msgi.body, contact_quoted=MYSELF, quote=msgi.quote_body, quote_date=quoted_msg.date, reaction=msgi.reactions))
           else:
-            html_result.write(build_msg(MYSELF, msg_date, msgi.body, contact_quoted=CONTACT_NAME, quote=msgi.quote_body, quote_date=quoted_msg.date, reaction=msgi.reactions))
+            html_result.write(build_msg(CONTACT_NAME, msg_date, msgi.body, contact_quoted=MYSELF, quote="NULL quote", quote_date=None, reaction=msgi.reactions))
+        # MMS is embedding a simple MMS without quoting
+        elif msgi.filename is not None:
+          html_result.write(build_msg(CONTACT_NAME, msg_date, msgi.body, filename=ATTACHMENT_DIR + msgi.filename, reaction=msgi.reactions))
         else:
-          html_result.write(build_msg(MYSELF, msg_date, msgi.body, contact_quoted=CONTACT_NAME, quote="NULL quote", quote_date=None, reaction=msgi.reactions))
-      # MMS is embedding a simple MMS without quoting
-      elif msgi.filename is not None:
-        html_result.write(build_msg(MYSELF, msg_date, msgi.body, filename=ATTACHMENT_DIR + msgi.filename, reaction=msgi.reactions))
+          raise ValueError
+  
+      # MMS sent
+      elif msgi.mms_type == SMS_SENT:
+        # MMS is quoting a msg
+        if msgi.quote_id > 0:
+          quoted_msg = msg_dict.get(msgi.quote_id)
+          # MMS can quote an SMS
+          if isinstance(quoted_msg, SMS):
+            html_result.write(build_msg(MYSELF, msg_date, msgi.body, contact_quoted=CONTACT_NAME, quote=msgi.quote_body, quote_date=quoted_msg.date, reaction=msgi.reactions))
+          # MMS can quote an MMS
+          elif isinstance(quoted_msg, MMS):
+            # MMS quote an MMS with an image
+            if quoted_msg.date in msg_dict.keys() and quoted_msg.filename is not None:
+              html_result.write(build_msg(MYSELF, msg_date, msgi.body, contact_quoted=CONTACT_NAME, filename=ATTACHMENT_DIR + quoted_msg.filename, quote= msgi.quote_body, quote_date=quoted_msg.date, reaction=msgi.reactions))
+            # MMS quote an MMS without an image
+            else:
+              html_result.write(build_msg(MYSELF, msg_date, msgi.body, contact_quoted=CONTACT_NAME, quote=msgi.quote_body, quote_date=quoted_msg.date, reaction=msgi.reactions))
+          else:
+            html_result.write(build_msg(MYSELF, msg_date, msgi.body, contact_quoted=CONTACT_NAME, quote="NULL quote", quote_date=None, reaction=msgi.reactions))
+        # MMS is embedding a simple MMS without quoting
+        elif msgi.filename is not None:
+          html_result.write(build_msg(MYSELF, msg_date, msgi.body, filename=ATTACHMENT_DIR + msgi.filename, reaction=msgi.reactions))
+        else:
+          raise ValueError
+  
+      elif msgi.mms_type in SMS_NULL:
+        pass
       else:
-        raise ValueError
-
-    elif msgi.mms_type in SMS_NULL:
-      pass
+        raise ValueError(msgi)
     else:
-      raise ValueError(msgi)
-  else:
-    raise ValueError
+      raise ValueError
+  
+  html_result.write(build_footer())
+  html_result.close()
 
-html_result.write(build_footer())
-html_result.close()
+if __name__ == "__main__":
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--db", dest="db_path", help="Path to signal_backup.db file", type=str)
+  parser.add_argument("--attachment", "-a", dest="attachment_dir", help="Path to attachment directory", type=str)
+  parser.add_argument("--thread", "-t", dest="thread_id", help="Conversation ID, multiple conversion saving are not yet supported", type=int)
+  parser.add_argument("--contact_addr", "-ca", dest="contact_address", help="Contact address, multiple conversion saving are not yet supported", type=int)
+  parser.add_argument("--contact_name", "-cn", dest="contact_name", help="Name of the contact you wish to display", type=str)
+  parser.add_argument("--you", "-m", dest="your_name", help="Your name", type=str)
+  parser.add_argument("--out", "-o", dest="html_output_file", help="html output file", type=str)
+  args = parser.parse_args()
+
+  THREAD_ID = args.thread_id
+  CONTACT_ADDRESS = args.contact_address
+  CONTACT_NAME = args.contact_name
+  MYSELF = args.your_name
+  ATTACHMENT_DIR = args.attachment_dir
+  DB_PATH = args.db_path
+  HTML_OUT = args.html_output_file
+  
+  conn = sqlite3.connect(DB_PATH)
+  db_cursor = conn.cursor()
+
+  msg_dict = OrderedDict(sorted(fetch_contact_msg(CONTACT_ADDRESS, db_cursor).items()))
+
+  save_msg(HTML_OUT, msg_dict)
+
+
+
