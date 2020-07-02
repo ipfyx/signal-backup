0032c4443a26ea5ba2c06ecbe30de9cc13393866
ipfyx <ipfyx>
2020-07-02 16:31:51 +0200

    most id in db should be casted in int

--- signal_structure.py
+++ signal_structure.py
@@ -9,15 +9,15 @@ SMS_NULL = [10747924,10747927,2,1,3]
 @total_ordering
 class MMS(object):
   def __init__(self, date, address, msg_type, body, quote_id, quote_author, quote_body, reactions, mms_id, part_ct, part_unique_id, part_quote):
-    self.date = date
-    self.address = address
-    self.msg_type = msg_type
+    self.date = int(date)
+    self.address = int(address)
+    self.msg_type = int(msg_type)
     if body:
       self.body = body
     else:
       self.body = ''
-    self.quote_id = quote_id
-    self.quote_author = quote_author
+    self.quote_id = int(quote_id)
+    self.quote_author = int(quote_author) if quote_author is not None else None
     self.quote_body = quote_body
     if reactions:
       self.reactions = reactions[:11].decode('utf-8')[4]
@@ -49,11 +49,11 @@ class MMS(object):
 @total_ordering
 class SMS(object):
   def __init__(self, thread_id, address, date, msg_type, body, reactions):
-    self.date = date
+    self.date = int(date)
     self.body = body 
-    self.msg_type = msg_type
-    self.thread_id = thread_id
-    self.address = address
+    self.msg_type = int(msg_type)
+    self.thread_id = int(thread_id)
+    self.address = int(address)
     if reactions:
       self.reactions = reactions[:11].decode('utf-8')[4]
       if self.reactions == '❤':
@@ -78,7 +78,7 @@ class SMS(object):
 
 class PART(object):
   def __init__(self, id_part, ct, unique_id, part_quote):
-    self.id_part = id_part 
+    self.id_part = int(id_part)
     self.ct = ct 
     self.unique_id = unique_id
 
@@ -100,11 +100,11 @@ class PART(object):
 
 class CONTACT(object):
   def __init__(self, _id, phone, color, name, thread_id):
-    self.id = _id
+    self.id = int(_id)
     self.name = name
     self.phone = phone
     self.color = color
-    self.thread_id = thread_id 
+    self.thread_id = int(thread_id)
 
   def __str__(self):
     return self.__repr__()
